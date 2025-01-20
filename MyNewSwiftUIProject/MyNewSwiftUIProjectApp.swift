2e2ba716167db0066755784296c54c9b26ae9802
GHOSTUSA <ethan.dahigermain@gmail.com>
2025-01-20 09:07:43 +0100

    Initial Commit

---
+++ MyNewSwiftUIProject/MyNewSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewSwiftUIProjectApp.swift
+//  MyNewSwiftUIProject
+//
+//  Created by Ethan DAHI-GERMAIN on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MyNewSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
