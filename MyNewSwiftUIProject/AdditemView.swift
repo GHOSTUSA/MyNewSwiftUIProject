c10bab268f5dff8c403e4bc85fb98d9c88f5d71f
GHOSTUSA <ethan.dahigermain@gmail.com>
2025-01-20 09:48:40 +0100

    MERDE

---
+++ MyNewSwiftUIProject/AdditemView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditemView.swift
+//  MyNewSwiftUIProject
+//
+//  Created by Ethan DAHI-GERMAIN on 1/20/25.
+//
+
+import SwiftUI
+
+struct AdditemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditemView()
+}
